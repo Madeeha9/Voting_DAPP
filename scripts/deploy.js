b3c934725ee1a86e0da782acaf1443ed33238465
Madeeha Ishaque <madeeha9@gmail.com>
2024-02-11 01:00:14 +0530

    smart contract commit

---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const vote = await ethers.getContractFactory("Voting");
+
+  const voting = await vote.deploy(
+    ["Thomas Shelby", "Ada Shelby", "Arthur Shelby", "John Shelby"],
+    60
+  );
+
+  console.log("Contract Address: ", voting.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
